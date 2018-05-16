2516c4ddb738f8e0da055135e3ab5257359b5896
an-ska <1.anna.piotrowska@gmail.com>
2018-05-16 20:42:46 +0200

    Alert styled

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import styles from './Alert.scss';
 
 const Alert = ({icon, text}) => (
-  <div>
-    <i className={`fas ${icon}`} />
-    {text}
+  <div className={styles.alert}>
+    <div className={styles.alertIcon}>
+      <i className={`fas fa-lg ${icon}`} />
+    </div>
+    <div className={styles.alertText}>{text}</div>
   </div>
 )
 
